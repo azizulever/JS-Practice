cf50a78d926cb4522ee89272da06b29b0f8c8dea
Azizul Islam <contact.azizulislam@gmail.com>
2024-02-22 22:40:20 +0600

    Practiced functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+    // console.log(a+b);
+    return a+b;
+}
+let result = sum(3, 5);
+console.log(result);
+
+let func = (a, b) => {
+    console.log(a+b);
+}
+
+func(3, 3);
+console.log(func);
+
